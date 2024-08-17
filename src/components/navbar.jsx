b7b56d98ce382369afa59c1da0824ca31dceaa65
Alexander Medvedev <mega.medved87@gmail.com>
2024-08-17 18:38:28 -0500

    Link Aliaksandr.dev

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,8 +1,16 @@
+import Link from "next/link"
+
 
 const Navbar = () => {
    return (
-     <div>
-       
+     <div className="h-full flex items-center justify-between px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48">
+        <div className="">
+            <Link href="/" className="text-sm rounded-md bg-black px-1 font-semibold flex items-center justify-center">
+            <span className="text-white mr-1">Aliaksandr</span>
+            <span className="w-12 h-8 rounded bg-white text-black flex items-center justify-center">.dev</span>
+            </Link>
+        </div>
+        <div className="">2</div>
      </div>
    )
  }
